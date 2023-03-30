d0fd782e57953779b7ba9e6170a4e2fb5fb50ebc
CCDM03 <ccdm03@CCDM03.local>
2023-03-30 09:46:14 -0700

    leer todos los registros

--- Index.js
+++ Index.js
@@ -1,3 +1,19 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'publicacion_de_blog'
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log('Error al conectarse a la base de datos:', error);
+  } else {
+    console.log('Conexión exitosa a la base de datos');
+  }
+});
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -5,6 +21,15 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+// Leer todos los registros
+app.get('/publicacion_de_blog', (req, res) => {
+    connection.query('SELECT * FROM publicacion_de_blog', (error, results) => {
+      if (error) throw error;
+      res.send(results);
+    });
+  });
+
 
 app.listen(3000, () => {
     console.log('API escuchando en el puerto 3000');
